f046bf4fea0a684966574b03db485a4bb6fbb05f
dipinsonwani <dipinsonwani@gmail.com>
2021-01-16 15:59:40 +0530

    Fetch Songs list from the server

--- lib/music_page.dart
+++ lib/music_page.dart
@@ -1,32 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:music_app/song.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class MusicPage extends StatefulWidget {
-  
   @override
   _MusicPageState createState() => _MusicPageState();
 }
 
 class _MusicPageState extends State<MusicPage> {
-
-  
+  var s;
   List<Song> songs = [
-    Song(songName: 'Popstar', artist: 'Rockstar', album: 'Trevor'),
-    Song(songName: 'Danza Kuduro', artist: 'Don Omar', album: 'Don Omar'),
-    Song(
-        songName: 'Paradise', artist: 'George Ezra', album: 'Staying at Tamara')
+    // Song(songName: 'Popstar', artist: 'Rockstar', album: 'Trevor'),
+    // Song(songName: 'Danza Kuduro', artist: 'Don Omar', album: 'Don Omar'),
+    // Song(
+    //     songName: 'Paradise', artist: 'George Ezra', album: 'Staying at Tamara')
   ];
-  void getSongs(){
-    Map data = ModalRoute.of(context).settings.arguments;
-    print(data);
+  Future<void> getList() async {
+    var response = await http.get(
+        'https://api.musixmatch.com/ws/1.1/chart.tracks.get?apikey=2d782bc7a52a41ba2fc1ef05b9cf40d7');
+    var data = json.decode(response.body) as Map<String, dynamic>;
+    List<Song> loadedSongs = [];
 
+    data.forEach((key, value) {
+
+      (value['body']['track_list'] as List<dynamic>).map((item) {
+        loadedSongs.add(Song(
+            songName: item['track']['track_name'],
+            album: item['track']['album_name'],
+            artist: item['track']['artist_name']));
+        print(item['track']['song_name']);
+      }).toList();
+
+    });
+    setState(() {
+      songs = loadedSongs;
+    });
+    
+    print(songs[0].songName);
   }
-  @override
-  void initState(){
-    super.initState();
-    Song s;
-    s.getList();
 
+
+  @override
+  void initState() {
+    getList();
+    super.initState();
   }
 
   @override
@@ -34,11 +52,21 @@ class _MusicPageState extends State<MusicPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Trending Music"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {
+              setState(() {
+                songs;
+              });
+            },
+          )
+        ],
       ),
       body: ListView.builder(
         itemBuilder: (context, index) {
           return Card(
-                      child: ListTile(
+            child: ListTile(
               title: Text(songs[index].songName),
               leading: Icon(Icons.music_note),
               subtitle: Text(songs[index].artist),
